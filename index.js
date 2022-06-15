784f3c029cba32c736c8bec2baf1aedf0f703db0
Wamuyu <michellewachira25@gmail.com>
2022-06-16 01:38:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+// Code your solution in this file
+let drivers = []
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2)
+
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Mike', 'Anna'])
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2)
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [][returnFirstTwoDrivers + returnLastTwoDrivers]
